3e6f61b45cfbe6cbddbf481136b7da0371e05dc1
biterpreter <matthew.x.holmes@gmail.com>
2017-03-31 15:39:04 -1000

    added old dependencies in case someone wants to use the old stuff

---
+++ .old/chatapi.py
@@ -0,0 +1,56 @@
+import requests
+import json
+
+class API:
+    def __init__(self, token):
+        self.baud_active = 1
+        self.baud_inactive = 5
+        self.token = token
+        self.usernames = {}
+        self.username = ''
+        self.channels = []
+        self.last_poll = 0
+        self.ready = False
+        self.chatbuffer = []
+
+
+    def send_request(self, method, params):
+        return requests.post('https://www.hackmud.com/mobile/{}.json'.format(method),
+                headers = {'Content-Type': 'application/json'},
+                json = params)
+
+    def login(self):
+        if len(self.token) > 6:
+            return self.get_usernames()
+        else:
+            res = self.send_request('get_token', { 'pass' : self.token } ).json()
+            self.token = res['chat_token']
+            return self.get_usernames()
+
+    def get_usernames(self):
+        res = self.send_request('account_data', { 'chat_token' : self.token }  ).json()
+        self.usernames = res['users']
+        self.ready = True
+        return res['users'].keys()
+
+    def set_username(self, username):
+        channels = self.usernames[username]
+        if len(channels):
+            self.username = username
+            self.channels = channels
+            return self.channels
+
+    def poll_messages(self):
+        if len(self.username) and self.ready:
+            res = self.send_request('chats', { 'chat_token' : self.token , 'usernames' : [self.username] } ).json()
+            try:
+                self.chatbuffer = res['chats'][self.username]
+            except:
+                pass
+            return reversed( self.chatbuffer )
+
+    def send_chat_to_user(self, username, msg):
+        return self.send_request('create_chat',  { 'chat_token' : self.token, 'username' : self.username, 'tell' : username, 'msg' : msg} ).json()
+
+    def send_chat_to_channel(self, channel, msg):
+        return self.send_request('create_chat',  { 'chat_token' : self.token, 'username' : self.username, 'channel' : channel, 'msg' : msg} ).json()
