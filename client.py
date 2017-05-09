4e0b404c4f0990f2aca8a7a4bc8e9730ae1d0af9
biterpreter <matthew.x.holmes@gmail.com>
2017-05-08 21:51:35 -0500

    Update client.py
    pathinggggg

--- client.py
+++ client.py
@@ -18,7 +18,7 @@ def main(stdscr):
 	token_file = Path(os.path.join(os.path.dirname(__file__),"token.dat"))
 	if token_file.is_file():
 		saved = True
-		token_file = open(token_file)
+		token_file = open(os.path.join(os.path.dirname(__file__),"token.dat"))
 		resp = token_file.read()
 		Api = chatapi.API(resp)
 		try:
